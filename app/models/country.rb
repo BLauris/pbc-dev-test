75dfc2a5af3730a9e9ca08c6937f4b09e30c48ca
BLauris <bligzna.lauris@gmail.com>
2017-10-24 17:25:32 +0300

    Added validations for `Country` model and wrote tests.

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,5 +1,13 @@
 class Country < ActiveRecord::Base
   
-  validate :country_code, :panel_provider_id, presence: true
+  validates :country_code, :panel_provider_id, presence: true
+  validates :country_code, length: { minimum: 2, maximum: 2 }
   
+  before_save :uppercase_country_code, if: Proc.new { |c| c.country_code_changed? && c.country_code.present? }
+  
+  private
+  
+    def uppercase_country_code
+      self.country_code = self.country_code.upcase
+    end
 end
--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Country, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "Happy Path" do
+    let(:country) { Country.new(country_code: "lv", panel_provider_id: 1) } # Hardcoded value `1` just to pass validation
+    
+    it "successfully add's 'country'" do
+      expect{
+        country.save 
+      }.to change{Country.count}.by(1)
+    end
+    
+    it "turns into uppercase letters 'country_code'" do
+      country.save
+      expect(country.country_code).to eq("LV")
+    end
+    
+    it "turns into uppercase letters 'country_code' on update" do
+      country.save
+      country.update(country_code: 'en')
+      expect(country.country_code).to eq("EN")
+    end
+  end
+  
+  context "Sad Path" do
+    let(:country) { Country.new }
+    
+    it "doesn't save 'country' with missing details" do
+      expect(country.save).to eq(false)
+      expect(country.errors.messages[:country_code]).to include("can't be blank")
+      expect(country.errors.messages[:panel_provider_id]).to include("can't be blank")
+    end
+      
+    it "validates length of 'country_code' to long" do
+      country.country_code = "LVA"
+      
+      expect(country.valid?).to eq(false)
+      expect(country.errors.messages[:country_code]).to include("is too long (maximum is 2 characters)")
+    end
+    
+    it "validates length of 'country_code' to short" do
+      country.country_code = "L"
+
+      expect(country.valid?).to eq(false)
+      expect(country.errors.messages[:country_code]).to include("is too short (minimum is 2 characters)")
+    end
+  end
+
 end
