28fd072275178255b8aaa29f8a3aef63a955231f
BLauris <bligzna.lauris@gmail.com>
2017-10-26 18:57:26 +0300

    Added `rate_limit_exceeded?` and `authenticate_user!` methods

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,25 @@
+class ApiController < ApplicationController
+  
+  protect_from_forgery with: :null_session
+  
+  private
+    
+    def user_exceeded_rate_limit?
+      unless Api::TokenService.decode!(token)
+        if Api::RateLimitService.exceeded?(ip)
+          render json: {message: "Rate limit exceeded!"}, status: 429 
+        end
+      end
+    end
+    
+    def authenticate_user!
+      unless Api::TokenService.decode!(token)
+        render json: {message: "You need to be authorized!"}, status: 401 
+      end
+    end
+  
+    def token
+      request.headers["token"]
+    end
+  
+end
