bbc1e7522e0b75c9dfa9ef1f80802105ae14c743
BLauris <bligzna.lauris@gmail.com>
2017-10-26 18:56:30 +0300

    Created controller for generating tokens

---
+++ app/controllers/api/tokens_controller.rb
@@ -0,0 +1,19 @@
+class Api::TokensController < ApiController
+  
+  def generate
+    token = Api::TokenService.generate_token!(user_params[:email])
+      
+    if token.present?
+      render json: { token: token }, status: 200
+    else
+      render json: { message: "User not found" }, status: 404
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:email)
+    end
+  
+end
