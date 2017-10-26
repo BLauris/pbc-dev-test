16c677fd9dcd1caa6520c91e99aa922522314904
BLauris <bligzna.lauris@gmail.com>
2017-10-26 18:53:43 +0300

    Wrote token service

---
+++ app/services/api/token_service.rb
@@ -0,0 +1,43 @@
+class Api::TokenService
+  
+  include Virtus.model(strict: true)
+  attribute :email, String
+  
+  def self.generate_token!(email)
+    token = self.new(email: email)
+    token.generate!
+  end
+  
+  def self.decode!(token)
+    decoded = JWT.decode(token, nil, false)
+    User.where(email: decoded.first["email"]).exists?
+  rescue
+    false
+  end
+  
+  def generate!
+    if user.present?
+      user.update(token: token, token_expires_at: token_expires_at)
+      user.token
+    end
+  end
+  
+  private
+  
+    def user
+      @user ||= User.find_by(email: email)
+    end
+    
+    def token
+      @token ||= token = JWT.encode(payload, nil, 'none')
+    end
+    
+    def payload
+      { email: user.email, exp: token_expires_at }
+    end
+    
+    def token_expires_at
+      @token_expires_at ||= DateTime.current.in_time_zone + 1.day
+    end
+    
+end
