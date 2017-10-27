af7d44d0f025f80e9471bb994a3a805326992aa3
BLauris <bligzna.lauris@gmail.com>
2017-10-27 12:36:56 +0300

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
 
   namespace :api, defaults: {format: 'json'} do    
-    resources :tokens, except: [:index, :show, :create, :new, :edit, :update, :destroy] do
-      collection do
-        get :generate
-      end
-    end
+    # resources :tokens, except: [:index, :show, :create, :new, :edit, :update, :destroy] do
+    #   collection do
+    #     get :generate
+    #   end
+    # end
     
-    resources :locations, only: [:show]
+    get 'tokens', to: 'tokens#generate', as: :token_generate_path
+    get 'locations/:country_code', to: 'locations#show', as: :location_path
+    get 'target_groups/:country_code', to: 'target_groups#show', as: :target_group_path
+    
     resources :evaluate_targets, only: [:create]
   end
 
